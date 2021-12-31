4ab10830e3a603d7e2d5302a2b67d848860b0233
Mohammad_Fahim_Hassan <mohammad.fahim018@northsouth.edu>
2021-12-31 19:03:59 +0600

    Added view function unittest

--- metro_rail/Ticket/tests.py
+++ metro_rail/Ticket/tests.py
@@ -1,6 +1,9 @@
 from django.test import SimpleTestCase
+from django.test import TestCase,Client
 from django.urls import reverse,resolve
 from .views import *
+from .models import *
+import json
 
 
 # Create your tests here.
@@ -18,4 +21,38 @@ class TestUrls(SimpleTestCase):
     def test_SeeComplains_url_is_resolved(self):
         url = reverse('SeeComplains')
         self.assertEquals(resolve(url).func, seecomplains)
+
+class TestViews(TestCase):
+
+    def setup(self):
+        self.client = Client()
+        self.aftercomplain_url = reverse('AfterComplain')
+
+    def test_complains(self):
+        client = Client()
+
+        response = client.get(reverse('Complains'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response,'Ticket/Complain.html')
+
+    def test_aftercomplains_Post(self):
+        self.client = Client()
+        self.aftercomplain_url = reverse('AfterComplain')
+
+        response= self.client.post(self.aftercomplain_url, {
+            'fname':'Anisha',
+            'contact':'get@gmail.com',
+            'subject':'Your Website is useless'
+        })
+
+    def test_seecomplains(self):
+        client = Client()
+
+        response = client.get(reverse('SeeComplains'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Ticket/SeeComplain.html')
+
+
 
